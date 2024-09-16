bbf6e44a55275346a5495c272fcb1a0a308a423e
studentlucas <338362@student.mboutrecht.nl>
2024-09-16 15:25:59 +0200

    init commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        about page
+    </x-slot:heading>
+    <h1>Hello from the about page</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        contact page
+    </x-slot:heading>
+    <h1>Hello from the contact page</h1>
+</x-layout>
